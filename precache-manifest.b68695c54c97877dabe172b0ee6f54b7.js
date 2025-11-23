9aa9114ac6b719e8bf2e3a107496b09f1a4f2eeb
gmralwnr <gmralwnr@hanmil.net>
2025-11-23 22:58:22 +0900

    Updates

---
+++ precache-manifest.b68695c54c97877dabe172b0ee6f54b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b43135e2ca0d091ea4cca39e13374c3",
+    "url": "/minji.portfolio.github.io/index.html"
+  },
+  {
+    "revision": "83069a7be8bcd6c711fb",
+    "url": "/minji.portfolio.github.io/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "aad56fb657c944953e66",
+    "url": "/minji.portfolio.github.io/static/css/main.7c40245c.chunk.css"
+  },
+  {
+    "revision": "83069a7be8bcd6c711fb",
+    "url": "/minji.portfolio.github.io/static/js/2.42f02fa0.chunk.js"
+  },
+  {
+    "revision": "0297ac78c823ef9b172e38637e06e92e",
+    "url": "/minji.portfolio.github.io/static/js/2.42f02fa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad56fb657c944953e66",
+    "url": "/minji.portfolio.github.io/static/js/main.c4bd75e5.chunk.js"
+  },
+  {
+    "revision": "0bef161056af43d2facf",
+    "url": "/minji.portfolio.github.io/static/js/runtime-main.bed45170.js"
+  }
+]);
